0612ba462c7663165b5511ec74efdfc4d67a998e
Kirill Smagin <k.smagin@cdek.ru>
2024-10-04 15:26:14 +0500

    1.0

--- src/main/java/com/cdek/international/customs/measurements/ui/PostamatConverter.java
+++ src/main/java/com/cdek/international/customs/measurements/ui/PostamatConverter.java
@@ -3,14 +3,22 @@ package com.cdek.international.customs.measurements.ui;
 import com.cdek.international.customs.measurements.core.domain.PostamatCell;
 import lombok.NonNull;
 import org.springframework.stereotype.Component;
+import tech.units.indriya.unit.Units;
+
+import javax.measure.Quantity;
 import java.util.stream.Collectors;
+
+import static javax.measure.MetricPrefix.CENTI;
+import static tech.units.indriya.unit.Units.METRE;
 
 @Component
 public class PostamatConverter {
     @NonNull
     public String toCellResponse(@NonNull PostamatCell cell) {
         return cell.dimensions().stream()
+                .map(dimension -> dimension.to(CENTI(Units.METRE)))
+                .map(Quantity::getValue)
                 .map(Object::toString)
-                .collect(Collectors.joining("x", "", " cm"));
+                .collect(Collectors.joining("x", "", " " + CENTI(METRE).toString()));
     }
 }
